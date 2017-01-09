204e39604092529baf3e85e09449858a7ba9230d
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-09 14:49:11 +0530

    Create PureStrDup.java

---
+++ PureStrDup.java
@@ -0,0 +1,34 @@
+package com.cogzidel.lk;
+
+import java.util.*;
+public class Sample{
+	
+
+		public static void main(String[] args) 
+		{
+			Scanner sc=new Scanner(System.in);
+			
+			String s=sc.next();
+			StringBuffer sb=new StringBuffer(s);
+			
+			for(int i=0;i<sb.length();i++)
+			{
+				int x=0;
+				char c=sb.charAt(i);
+				while(sb.indexOf(String.valueOf(c))!=sb.lastIndexOf(String.valueOf(c)))
+				{
+					sb.deleteCharAt(sb.lastIndexOf(String.valueOf(c)));
+					x=1;
+				}
+				if(x==1)
+				{
+					sb.deleteCharAt(sb.indexOf(String.valueOf(c)));
+					i--;
+				}
+			}
+			System.out.println(sb);
+		}
+
+	}
+	
+	
