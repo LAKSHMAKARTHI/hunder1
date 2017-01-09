09645f729f983c0c94c957caec54402f88627254
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-09 14:15:46 +0530

    Update Substr.java

--- Substr.java
+++ Substr.java
@@ -1,24 +1,23 @@
-import java.io.*;
+package com.cogzidel.lk;
+
 import java.util.*;
-class Substring
-{
- public static void main(String args[])
- {
-int i,n=0;
-String b="";
-  Scanner s=new Scanner(System.in);
-  String a=s.next();
-  for(i=0;i<a.length();i++)
-  {
-    n=n+Character.getNumericValue(a.charAt(i));
-  }
-  b=String.valueOf(n);
-  StringBuffer c=new StringBuffer(b);
-  c.reverse();
-  String d=new String(c);
-  if(b.equals(d))
-     System.out.println("palindrome");
-  else
-    System.out.println("not palindrome");
- }
-}
+public class Sample {
+
+	
+	  public static void main(String args[]){
+	    Scanner in=new Scanner(System.in);
+	    String s="";
+	    System.out.println("enter the string :");
+	    s=in.nextLine();
+	    if(s.length()%2==0)
+	    {
+	      System.out.println("the maximum length of non palindrome substring :"+s.length());
+	    }
+	    else
+	    {
+	      System.out.println("the maximum length of non palindrome substring is :"+s.length()+1);
+	    }
+	  }
+	}
+
+	   
